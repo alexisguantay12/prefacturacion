2f7fcefff6e087866f185447891dad20c9b91c06
alexissaltacode <alexisguantay@saltacode.com>
2026-07-06 15:45:04 -0300

    lla

--- applications/core/views.py
+++ applications/core/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
 @login_required
 def home_view(request):
-    return render(request,'inicio/inicio.html')
+    return redirect("gestion_app:lista_preingresos")
--- applications/gestion/views/home.py
+++ applications/gestion/views/home.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, "gestion/orden_list.html")
+    return render(request, "gestion/preingreso/lista_preingresos.html")
